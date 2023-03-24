94320f22135fef2b24b141938a64f182632d5554
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-03-24 17:25:05 +0100

    Add root route handler

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
-import express from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 import initiate from "./db/db";
 
-const app = express();
+const app: Application = express();
 const PORT = Number(process.env.PORT || 6000);
 
 app.use(cors());
 app.use(express.json());
 
 initiate();
+
+app.get("/", (req: Request, res: Response) => {
+	return res.status(200).json({ message: "Chat API is Running!!!" });
+});
 
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`);
